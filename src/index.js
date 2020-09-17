0c9f075789692c00fbc3d365fee1baedb378f943
Akanibekuly <a.kanibekuly@gmail.com>
2020-09-17 13:32:51 +0600

    feat: solved typical array problem

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array==null||array.length==0){
+    return 0;
+  }
+  var min=array[0];
+  for(var i=0;i<array.length;i++){
+    if(array[i]<min){
+      min=array[i];
+    }
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array==null||array.length==0){
+    return 0;
+  }
+  var max=array[0];
+  for(var i=0;i<array.length;i++){
+    if(max<array[i]){
+      max=array[i];
+    }
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array==null||array.length==0){
+    return 0;
+  }
+  var avg=0;
+  for(var i=0;i<array.length;i++){
+    avg+=array[i];
+  }
+  return avg/array.length;
 }
